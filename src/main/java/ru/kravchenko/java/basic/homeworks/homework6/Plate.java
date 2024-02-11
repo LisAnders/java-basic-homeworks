29e8df048d674db051598b14157e85ba3669bac8
LisAnders <denis-krv93@yandex.ru>
2024-02-11 13:53:55 +0300

    Решение по ДЗ 6

---
+++ src/main/java/ru/kravchenko/java/basic/homeworks/homework6/Plate.java
@@ -0,0 +1,32 @@
+package ru.kravchenko.java.basic.homeworks.homework6;
+
+public class Plate {
+    private int maxFoodQuant;
+    private int currentFoodQuant;
+
+    public Plate(int maxFoodQuant) {
+        this.maxFoodQuant = maxFoodQuant;
+        this.currentFoodQuant = maxFoodQuant;
+    }
+
+    public void addFoodInPlate(int foodQuant) {
+        if (this.currentFoodQuant + foodQuant < this.maxFoodQuant) {
+            this.currentFoodQuant += foodQuant;
+        } else {
+            this.currentFoodQuant = this.maxFoodQuant;
+        }
+    }
+
+    public boolean decreaseFoodInPlate(int eatQuant) {
+        if ((this.currentFoodQuant - eatQuant) >= 0) {
+            this.currentFoodQuant -= eatQuant;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void info() {
+        System.out.println("В тарелке осталось " + this.currentFoodQuant + " еды");
+    }
+}
